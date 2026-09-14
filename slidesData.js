736cfdaeb8d72f0841859ff35874497caf765a22
nitsugwen <gwenstefaniagustin@gmail.com>
2026-09-15 06:06:12 +0800

    Update slidesData.js

--- slidesData.js
+++ slidesData.js
@@ -4,19 +4,19 @@ const carouselSlidesData = [
     category: "Portraits",
     title: "Leon S. Kennedy",
     description: "A rugged 51-year-old battle-hardened veteran federal agent.",
-    image: "https://i.redd.it/the-coolest-female-and-male-character-in-resident-evil-v0-eydr7n019h6h1.jpg?width=3840&format=pjpg&auto=webp&s=012c5fc50cddc3d21dc82e7f8200d796fe188d76",
+    image: "photos/leon.jpg",
   },
   {
     category: "Portraits",
     title: "Edward James Kenway",
     description: "A pirate captain during the Golden Age of Piracy, he sails the Caribbean on his ship, the Jackdaw.",
-    image: "https://i.redd.it/close-up-shots-of-edward-v0-fwuwqb1u8tzg1.jpg?width=3840&format=pjpg&auto=webp&s=32288a8674e9c949db8812e17d85f958e67ee2be"
+    image: "photos/kenway.jpg"
   },
   {
     category: "Portraits",
     title: "Wrench",
     description: "The top engineer and fixer for the San Francisco branch of the hacker group DedSec.",
-    image: "https://images4.alphacoders.com/775/775646.jpg"
+    image: "photos/wrench.jpg"
   },
 
   // TRAVEL
@@ -24,19 +24,19 @@ const carouselSlidesData = [
     category: "Travel",
     title: "Leave her, Johnny",
     description: "The anchor's on board and the cable's all stored to be rollicking randy dandy-oh!",
-    image: "https://cdn.wccftech.com/wp-content/uploads/2018/01/seaofthieves_beach-scaled.jpg"
+    image: "photos/seaofthieves.jpg"
   },
   {
     category: "Travel",
     title: "Dragonspine",
     description: "A vast, snow-capped mountain region historically known as Vindagnyr.",
-    image: "https://i.redd.it/some-insanely-beautiful-visuals-that-i-compiled-from-the-v0-1q2n1s8kgo461.jpg?width=2048&format=pjpg&auto=webp&s=865c8bbaed26c2596f30d8b62cef73c093bed574"
+    image: "photos/dragonspine.jpg"
   },
   {
     category: "Travel",
     title: "New York City",
     description: "Known for its massive skyscrapers, non-stop energy, and Spider-Man.",
-    image: "https://i.redd.it/just-discovered-photo-mode-in-spider-man-remastered-and-im-v0-le5g87txhtye1.jpg?width=3840&format=pjpg&auto=webp&s=dbf84130f832cbc777489c1676b7add17b0b45e7"
+    image: "photos/nyc.jpg"
   },
 
   // EVENTS
@@ -44,19 +44,19 @@ const carouselSlidesData = [
     category: "Events",
     title: "Paranoia MV",
     description: "Blending genres, breaking limits. Step into the world of HEARTSTEEL.",
-    image: "https://wallpapercave.com/wp/wp13144610.jpg"
+    image: "photos/heartsteel.jpg"
   },
   {
     category: "Events",
     title: "Saja Boys Debut",
     description: "Saja Boys ready to steal your heart (and maybe your soul) with debut single 'Soda Pop'",
-    image: "https://img.uhdpaper.com/wallpaper/kpop-demon-hunters-saja-boys-660@5@f"
+    image: "photos/sajaboys.jpg"
   },
   {
     category: "Events",
     title: "Vocaloid Live Concert",
     description: "Lights up for the Cryptonloid Gang.",
-    image: "https://scontent.fcrk1-2.fna.fbcdn.net/v/t39.30808-6/668860052_1501009531389709_6498765460335919906_n.jpg?stp=dst-jpg_tt6&cstp=mx2048x1365&ctp=s2048x1365&_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_ohc=Ngb6IDHkaAgQ7kNvwG8NBYO&_nc_oc=AdpyF7ew7z68SqEoWJMUhS0H4uE1w5tf2I-Cfl-9AkJZm3Qgqv-pndlCo6Z0D3fs3m4&_nc_zt=23&_nc_ht=scontent.fcrk1-2.fna&_nc_gid=DJDherxreqcxcDgxYEE9xQ&_nc_ss=7b289&oh=00_AQLnve4G3EWVES_m_Lm1p5MvOQxXsdF1NCaMi_8UIYDVtg&oe=6AAE1E2F"
+    image: "photos/vocaloid.jpg"
   },
 
   // NATURE
@@ -64,18 +64,18 @@ const carouselSlidesData = [
     category: "Nature",
     title: "Kelp Forest",
     description: "Shining creepvines and Stalker teeth: A beautiful deathtrap.",
-    image: "https://i.redd.it/l02ryhzv81i41.jpg"
+    image: "photos/kelp.jpg"
   },
   {
     category: "Nature",
     title: "Kyrat",
     description: "Breathtaking Himalayan paradise of soaring, snow-capped peaks and lush hidden valleys.",
-    image: "https://images5.alphacoders.com/532/thumb-1920-532565.jpg"
+    image: "photos/kyrat.jpg"
   },
   {
     category: "Nature",
     title: "Endor",
     description: "Ancient redwoods and lush, pristine wilderness that conceals a galactic battleground beneath its peaceful canopy.",
-    image: "https://wallpapercave.com/wp/wp10707839.jpg"
+    image: "photos/endor.jpg"
   },
 ];
